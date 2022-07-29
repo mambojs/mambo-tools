9f4a88ed974a31fa9ea1f35efbf2c6e9ab228603
FrancoGit10 <43768757+FrancoGit10@users.noreply.github.com>
2022-07-29 18:32:59 -0300

    add linters (#13)

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 11,
+	},
+	rules: {
+		indent: [
+			"error",
+			"tab",
+			{
+				SwitchCase: 1,
+				ignoredNodes: ["ConditionalExpression"],
+			},
+		],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+};
