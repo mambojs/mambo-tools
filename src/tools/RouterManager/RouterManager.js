fced5086c54cf48b1df5d7cc0d4ee2041dacb82c
javierbasualdo <javierbasualdocom@gmail.com>
2022-12-20 17:16:58 -0300

    new configs and events

--- src/tools/RouterManager/RouterManager.js
+++ src/tools/RouterManager/RouterManager.js
@@ -1,24 +1,23 @@
-tools.class.RouterManager = function RouterManager() {
+tools.class.RouterManager = function RouterManager(props) {
 	const self = this;
 	window.addEventListener("locationchange", setRoute);
 
+	let m_props;
 	let current = {
 		name: "",
 		path: "",
-		from: {
-			name: "",
-			path: "",
-		},
-		to: {
-			name: "",
-			path: "",
-		},
+		from: {},
+		to: {},
 		params: {},
 		query: "",
 	};
 
 	let historyManager;
-	let routesList = [];
+	let m_routesList = [];
+	let m_eventListener;
+	let m_routerEvents = [{ name: "onBeforeRouteUpdate", used: false }];
+	let m_lastRoute = {};
+	let m_newMatchedObject;
 
 	this.add = addRoutes;
 	this.back = routerBack;
@@ -29,9 +28,37 @@ tools.class.RouterManager = function RouterManager() {
 	this.push = routerPush;
 	this.replace = routerReplace;
 	this.routes = getSetRoutes;
+	this.init = init;
+
+	setup();
+
+	function setup() {
+		config(props);
+		prepareEvents();
+		getSetRoutes(m_props.routes);
+	}
+
+	function init() {
+		if (!historyManager) {
+			self.current.path = location.pathname;
+			const { matched, path, newMatchedObject, originalObject } = matchedRouteBy({ path: location.pathname });
+			setRouteTo(originalObject);
+
+			if (path !== "/") {
+				updateCurrent(newMatchedObject);
+			}
+
+			historyManager = tools.history(path);
+
+			if (path === "/") {
+				execDispatch("onBeforeRouteUpdate", self.current, newMatchedObject, path);
+			}
+		}
+	}
 
 	function addRoutes(args) {
 		if (args.constructor.name === "Array" && args.length) {
+
 			if (!checkRoutesFormat(args)) {
 				return;
 			}
@@ -44,14 +71,14 @@ tools.class.RouterManager = function RouterManager() {
 				if (route.constructor.name === "Object") {
 					let routeExist = false;
 
-					routesList.forEach((r) => {
+					m_routesList.forEach((r) => {
 						if (r.path === route.path || r.alias === route.path) {
 							routeExist = true;
 						}
 					});
 
 					if (!routeExist) {
-						routesList.push(route);
+						m_routesList.push(route);
 					}
 				}
 			});
@@ -81,40 +108,45 @@ tools.class.RouterManager = function RouterManager() {
 		return true;
 	}
 
+	function checkBasePath(path) {
+		if (m_props.basePath.length) {
+			let newPaths = [];
+
+			m_props.basePath.forEach((base) => {
+				if (base.charAt() !== "/") {
+					base = base.replace(/^/, "/");
+				}
+
+				newPaths.push(`${base}${path}`);
+			});
+
+			if (!m_props.baseStrict) {
+				newPaths.push(path);
+			}
+
+			return newPaths;
+		} else {
+			return path;
+		}
+	}
+
 	function getSetRoutes(args) {
 		// Get
 
 		if (!args) {
-			return routesList;
+			return m_routesList;
 		}
 
 		// Set
 		// Check objects format, Check duplicated name or path, Add routes to list, Init router/history
 
-		if (routesList.length > 0) {
+		if (m_routesList.length > 0) {
 			addRoutes(args);
 			return;
 		}
 
 		if (Array.isArray(args) && args.length) {
-			if (!checkRoutesFormat(args)) {
-				return;
-			}
-
-			if (!checkRoutesDuplicated(args)) {
-				return;
-			}
-
-			routesList = args.concat(routesList);
-
-			if (!historyManager) {
-				const { matched, path } = matchedRouteBy({ path: location.pathname });
-
-				if (matched) {
-					historyManager = tools.history(path);
-				}
-            }
-
+			addRoutes(args);
 			return;
 		}
 	}
@@ -126,7 +158,7 @@ tools.class.RouterManager = function RouterManager() {
 
 	function isValidRouteObject(args) {
 		// Check if .rutes() is empty, Check if args is Object
-		if (!routesList.length) return false;
+		if (!m_routesList.length) return false;
 
 		if (args && args.constructor.name === "Object") {
 			// Allow path/name/params/query/hash, Only strings & object values
@@ -160,24 +192,47 @@ tools.class.RouterManager = function RouterManager() {
 	}
 
 	function matchedRouteBy({ path, name }) {
-		const routeMatched = routesList.find((route) => route.path === path || route.path + "/" === path || route.name === name);
+
+		if (m_props.basePath.length && name) {
+			console.error("When basePath is true, {name} param not run. Please, change BaseStrict to false or send {path} param");
+			return { matched: false };
+		}
+
+		const originalMatched = m_routesList.find((route) => {
+			return (
+				route.path === path ||
+				route.path + "/" === path ||
+				route.alias === path ||
+				route.alias === path + "/" ||
+				route.name === name
+			);
+		});
+
+		const routeMatched = m_routesList.find((route) => {
+			return checkBasePath(route.path).includes(path) || checkBasePath(route.path + "/").includes(path) || route.name === name;
+		});
 
 		if (routeMatched) {
-			return { matched: true, path: routeMatched.path };
+			path = path || routeMatched.path;
+			return { matched: true, path, newMatchedObject: updatePath(routeMatched, path), originalObject: originalMatched || {} };
 		}
 
 		if (path) {
-			const hasAliases = routesList.find((route) => route.alias === path || route.alias === path + "/");
+			const hasAliases = m_routesList.find((route) => {
+				if (route.alias) {
+					return checkBasePath(route.alias).includes(path) || checkBasePath(route.alias).includes(path + "/");
+				}
+			});
 
 			if (hasAliases) {
-				return { matched: true, path: hasAliases.alias };
+				return { matched: true, path, newMatchedObject: updatePath(hasAliases, path), originalObject: originalMatched || {} };
 			}
 		}
 
-		const hasNotFound = routesList.find((route) => route.notfound);
+		const hasNotFound = m_routesList.find((route) => route.notfound);
 
 		if (hasNotFound) {
-			return { matched: true, path: hasNotFound.path };
+			return { matched: true, path, newMatchedObject: updatePath(hasNotFound, path), originalObject: originalMatched || {} };
 		}
 
 		return { matched: false };
@@ -196,15 +251,20 @@ tools.class.RouterManager = function RouterManager() {
 		historyManager.go(args);
 	}
 
-	function routerPush(routeObject) {
+	function routerPush(routeObject, dispatch = true) {
 		if (isValidRouteObject(routeObject)) {
 			if (isCurrentRoute(routeObject)) return;
-
-			const { matched, path } = matchedRouteBy(routeObject);
+			const { matched, path, newMatchedObject, originalObject } = matchedRouteBy(routeObject);
 
 			if (matched) {
-				updateCurrent(routeObject, true);
-				historyManager.pushState(path, "", path);
+				setRouteFrom();
+				setRouteTo(originalObject);
+
+				if (dispatch) {
+					execDispatch("onBeforeRouteUpdate", self.current, newMatchedObject, path);
+				} else {
+					goToNewRoute(newMatchedObject, path);
+				}
 			}
 		}
 	}
@@ -216,14 +276,18 @@ tools.class.RouterManager = function RouterManager() {
 	function runAction() {
 		if (Object.prototype.hasOwnProperty.call(self.current, "action")) {
 			if (self.current.action.constructor.name === "Function") {
-				self.current.action();
+				self.current.action(self);
 			}
 		}
 	}
 
+	function goToNewRoute(newRouteObject, path) {
+		updateCurrent(newRouteObject);
+		historyManager.pushState(path, "", path);
+	}
+
 	function setRoute() {
-		const currentRouteObject = routesList.find((route) => route.path === history.state || route.alias === history.state);
-		updateCurrent(currentRouteObject);
+		setLastRoute();
 		runAction();
 	}
 
@@ -233,6 +297,77 @@ tools.class.RouterManager = function RouterManager() {
 		}
 
 		self.current = tools.utils().extend(true, self.current, currentRouteObject);
+	}
+
+	function updatePath(routeObject, path) {
+		return tools.utils().extend(true, routeObject, { path });
+	}
+
+	function prepareEvents(options) {
+		m_eventListener = document.createElement("div");
+
+		let eventObject = {};
+
+		m_routerEvents.forEach((event) => {
+			eventObject[event.name] = (cbk) => {
+				event.used = true;
+				addListeners(event.name, (res) => cbk(res.to, res.from, next));
+			};
+		});
+
+		m_props.events(eventObject);
+	}
+
+	function addListeners(event, cbk) {
+		m_eventListener.addEventListener(event, (x) => cbk(x.detail));
+	}
+
+	function execDispatch(event, currentObject, newMatchedObject, path) {
+		m_newMatchedObject = newMatchedObject;
+		const evtObject = m_routerEvents.find((evt) => evt.name === event);
+
+		if (evtObject.used) {
+			m_eventListener.dispatchEvent(new CustomEvent(event, { detail: currentObject }));
+		} else {
+			setRouteTo({});
+			goToNewRoute(m_newMatchedObject, path);
+		}
+	}
+
+	function setLastRoute() {
+		const simpleRoute = tools.utils().extend(true, {}, self.current);
+		delete simpleRoute.from;
+		delete simpleRoute.to;
+		m_lastRoute = tools.utils().extend(true, m_lastRoute, simpleRoute);
+	}
+
+	function setRouteFrom(routeObject) {
+		self.current.from = m_lastRoute;
+	}
+
+	function setRouteTo(originalObject) {
+		self.current.to = originalObject;
+	}
+
+	function next(params) {
+		if (params) {
+			self.push(params, false);
+			return;
+		}
+
+		let path = self.current.to.path;
+		setRouteFrom();
+		setRouteTo({});
+		goToNewRoute(m_newMatchedObject, path);
+	}
+
+	function config(props = {}) {
+		m_props = {
+			basePath: [],
+			baseStrict: false,
+		};
+
+		m_props = tools.utils().extend(true, m_props, props);
 	}
 };
 
